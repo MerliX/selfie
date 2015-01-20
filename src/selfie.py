a7940df4d33040d104e345d1ae2daa133b917388
Alexey Kirpichnikov <alexkir@skbkontur.ru>
2015-01-21 02:17:12 +0500

    sql optimizations

--- src/selfie.py
+++ src/selfie.py
@@ -186,9 +186,14 @@ def do_delete_requirement():
 @view('moderator_tasks')
 @check_moderator
 def moderator_tasks():
+    Assignee = User.alias()
+    Partner = User.alias()
     return {
         'selfies': Task
-                   .select()
+                   .select(Task, Assignee, Partner)
+                   .join(Assignee, on=(Task.assignee == Assignee.id))
+                   .switch(Task)
+                   .join(Partner, on=(Task.partner == Partner.id))
                    .where(
                         (Task.is_approved == False) 
                         & (Task.is_selfie_game == True)
