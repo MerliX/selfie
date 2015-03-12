7154965aacdbb71bf95d791f8d5523e52c72bfe6
Alexey Kirpichnikov <alexkir@skbkontur.ru>
2015-03-12 19:08:40 +0500

    update recreate_db script to work with postgres

--- recreate_db.py
+++ recreate_db.py
@@ -1,8 +1,12 @@
 from src.models import db, User, Requirement, Task
 
+db.connect()
+
 try:
     db.drop_tables([User, Requirement, Task])
 except:
-    pass
+    db.rollback()
 
 db.create_tables([User, Requirement, Task])
+
+db.close()
