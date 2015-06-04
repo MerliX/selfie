0b7d9527a7d56cb99deae48f9bd1750e064c064d
Alexey Kirpichnikov <alexkir@skbkontur.ru>
2015-06-04 16:30:50 +0500

    automatically create first task with fixture users

--- recreate_db.py
+++ recreate_db.py
@@ -12,4 +12,12 @@ with open('fixtures/users.csv') as f:
         userdata = line.rstrip().split(';')
         username = userdata[0].split()
         username = '%s %s' % (username[1], username[0])
-        User(name=username, access_code=userdata[3]).save()
+
+        user = User(name=username, access_code=userdata[3])
+        user.save()
+
+        Task(
+            assignee=user,
+            description=u'Сделай селфи, на котором хорошо видно лицо.',
+            difficulty=0
+        ).save()
