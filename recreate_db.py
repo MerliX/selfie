9b4ea9437c128cc274a79b3b4b0c37cfeaf1ea8c
Alexey Kirpichnikov <alexkir@skbkontur.ru>
2015-06-04 15:53:17 +0500

    add test user fixtures

--- recreate_db.py
+++ recreate_db.py
@@ -6,3 +6,10 @@ except:
     pass
 
 db.create_tables([User, Requirement, Task])
+
+with open('fixtures/users.csv') as f:
+    for line in f:
+        userdata = line.split(';')
+        username = userdata[0].split()
+        username = '%s %s' % (username[1], username[0])
+        User(name=username, access_code=userdata[3]).save()
