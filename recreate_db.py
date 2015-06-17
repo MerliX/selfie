7bf2a76903aff5cdb885b3b4e3eb84124f7adce9
Alexey Kirpichnikov <alexkir@skbkontur.ru>
2015-06-17 13:23:05 +0500

    fix stupid variable naming mistake

--- recreate_db.py
+++ recreate_db.py
@@ -14,9 +14,9 @@ with open('fixtures/users.csv') as f:
     for line in f:
         userdata = line.rstrip().split(';')
         username = userdata[0].split()
-        username = '%s %s' % (username[1], username[0])
+        shortname = '%s %s' % (username[1], username[0])
 
-        user = User(name=username, access_code=userdata[3])
+        user = User(name=shortname, access_code=userdata[3])
         try:
             user.save()
         except IntegrityError:
