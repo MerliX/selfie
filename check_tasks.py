d4327f9fd3a226141423ede11998b5fe2e260f0c
Alexey Kirpichnikov <alexkir@skbkontur.ru>
2015-06-19 00:40:05 +0500

    fix check_tasks script

--- check_tasks.py
+++ check_tasks.py
@@ -1,9 +1,14 @@
 # -*- coding: utf-8 -*-
 
-from src.models import Task
+import logging
+from src.models import Task, User
 
+logger = logging.getLogger('peewee')
+logger.setLevel(logging.ERROR)
 
 t = Task()
+u = User()
+t.assignee = u
 
 for i in range(15):
     for j in range(5):
